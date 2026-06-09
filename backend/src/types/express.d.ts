115dec48d5f8feeec711bf078a5066f00e62a64c
RestlessCoder <nick.limca@hotmail.com>
2026-06-09 06:25:00 +0100

    Include src/types in tsconfig to resolve req.user augmentation

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,12 +1,9 @@
 // express.d.ts gives you autocomplent(req.user.email) and type safefty (Indusry standard)
 
-export {};
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: { id: number; email: string; siteId: number; role: "ADMIN" | "USER"; isVerified: boolean }; 
-    }
+declare namespace Express  {
+  interface Request {
+    user?: { id: number; email: string; siteId: number; role: "ADMIN" | "USER"; isVerified: boolean }; 
   }
 }
 
